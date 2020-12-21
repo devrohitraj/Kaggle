c59731d710175e0d9892d2e37d9d733da7a37d5c
Rohit Raj <devrohitraj@users.noreply.github.com>
2020-12-21 12:52:40 +0530

    Update test_cal.py

--- test_cal.py
+++ test_cal.py
@@ -1,8 +1,13 @@
-def add(a,b):
-    return a+b
-def subtract(a,b):
-    return a-b
-def multiply(a,b):
-    return a*b
-def divide(a,b):
-    return a/b
+import Calculator
+def test_add():
+    total=Calculator.add(4,5)
+    assert total==9
+def test_subtract():
+    total=Calculator.subtract(9,5)
+    assert total==4
+def test_multiply():
+    total=Calculator.multiply(10,3)
+    assert total==30
+def test_divide():
+    total=Calculator.divide(9,3)
+    assert total==3
